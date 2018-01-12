52cafdbdbf179d8e20276f109ff7cff734a71ffb
ahaczyk <haczyslaw@gmail.com>
2018-01-12 15:11:10 +0100

    Add sidekiq production config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,13 @@
+if Rails.env.production?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDIS_URL'] }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDIS_URL'] }
+  end
+end
+
 schedule_file = Rails.root.join('config/schedule.yml')
 
 if File.exist?(schedule_file) && Sidekiq.server?
